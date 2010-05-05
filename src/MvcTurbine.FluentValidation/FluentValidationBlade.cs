70a11c6074ec61cb4d13df64a751a5f29656b0ee
unknown <cauthond@.DEG.local>
2010-05-05 10:27:04 -0500

    Make the validation blade scan for all validators and add them to the factory.

--- src/MvcTurbine.FluentValidation/FluentValidationBlade.cs
+++ src/MvcTurbine.FluentValidation/FluentValidationBlade.cs
@@ -1,4 +1,8 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web.Mvc;
 using FluentValidation.Mvc;
 using MvcTurbine.Blades;
 
@@ -27,7 +31,39 @@ namespace MvcTurbine.FluentValidation
         {
             var serviceLocator = context.ServiceLocator;
             var validatorFactory = new ServiceLocatorValidatorFactory(serviceLocator);
+
+            var retriever = new ValidatorRetriever();
+            foreach (var type in retriever.GetAllMessageHandlerTypes())
+                validatorFactory.AddValidatorToBeResolved(type);
+
             return new FluentValidationModelValidatorProvider(validatorFactory);
+        }
+
+        private class ValidatorRetriever
+        {
+            public IEnumerable<Type> GetAllMessageHandlerTypes()
+            {
+                var list = new List<Type>();
+
+                foreach (var assembly in GetAllAssemblies())
+                    list.AddRange(GetAllMessageHandlersInThisAssembly(assembly));
+
+                return list;
+            }
+
+            private static IEnumerable<Type> GetAllMessageHandlersInThisAssembly(Assembly assembly)
+            {
+
+                return assembly.GetTypes()
+                    .Where(x => x.GetInterfaces() != null && x.GetInterfaces().Any(i => (i.FullName ?? string.Empty).StartsWith("FluentValidation.IValidator`1")));
+            }
+
+            private static IEnumerable<Assembly> GetAllAssemblies()
+            {
+                return AppDomain.CurrentDomain.GetAssemblies()
+                    .Where(x => x.FullName.StartsWith("FluentValidation,") == false)
+                    .ToList();
+            }
         }
     }
 }
