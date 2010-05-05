58a080ef5d1ca6cdaea53629e321cb2ffa7a6574
unknown <cauthond@.DEG.local>
2010-05-05 09:55:03 -0500

    Implement the fluent validation blade.

--- src/MvcTurbine.FluentValidation/FluentValidationBlade.cs
+++ src/MvcTurbine.FluentValidation/FluentValidationBlade.cs
@@ -1,4 +1,6 @@
-﻿using MvcTurbine.Blades;
+﻿using System.Web.Mvc;
+using FluentValidation.Mvc;
+using MvcTurbine.Blades;
 
 namespace MvcTurbine.FluentValidation
 {
@@ -6,6 +8,26 @@ namespace MvcTurbine.FluentValidation
     {
         public override void Spin(IRotorContext context)
         {
+            StopMvcFromRequiringAllNonNullFields();
+            AddAFluentValidationModelValidatorProvider(context);
+        }
+
+        private static void StopMvcFromRequiringAllNonNullFields()
+        {
+            DataAnnotationsModelValidatorProvider.AddImplicitRequiredAttributeForValueTypes = false;
+        }
+
+        private static void AddAFluentValidationModelValidatorProvider(IRotorContext context)
+        {
+            var fluentValidationModelValidatorProvider = CreateFluentValidationModelValidatorProvider(context);
+            ModelValidatorProviders.Providers.Add(fluentValidationModelValidatorProvider);
+        }
+
+        private static FluentValidationModelValidatorProvider CreateFluentValidationModelValidatorProvider(IRotorContext context)
+        {
+            var serviceLocator = context.ServiceLocator;
+            var validatorFactory = new ServiceLocatorValidatorFactory(serviceLocator);
+            return new FluentValidationModelValidatorProvider(validatorFactory);
         }
     }
 }
